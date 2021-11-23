ddd146d625e8ad88eaeeaf873f5959b89719468f
晚安 <a103308928@gmail.com>
2021-11-23 16:23:58 +0800

    20211123打卡

--- sql/src/com/hrbu/Day13_每位学生的最高成绩_1.sql
+++ sql/src/com/hrbu/Day13_每位学生的最高成绩_1.sql
@@ -43,3 +43,31 @@ Result 表：
 原文链接：https://blog.csdn.net/weixin_39032019/article/details/121477225
 
 */
+
+create table Enrollments
+(
+    student_id   int ,
+    course_id    int ,
+    grade        int
+) ;
+
+insert into Enrollments (student_id , course_id , grade) values
+(2          , 2         , 95   ),
+(2          , 3         , 95   ),
+(1          , 1         , 90   ),
+(1          , 2         , 99   ),
+(3          , 1         , 80   ),
+(3          , 2         , 75   ),
+(3          , 3         , 82   );
+
+select student_id,course_id,grade
+from
+(
+select student_id
+       ,course_id
+       ,grade
+       ,row_number() over(partition by student_id order by grade desc,course_id) as rn
+  from Enrollments
+) t
+where t.rn = 1
+order by t.student_id
