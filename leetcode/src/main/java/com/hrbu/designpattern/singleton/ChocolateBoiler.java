a72b59484d6cdc19c9c9a8f2a221cf2e85d96113
晚安 <a1033078928@gmail.com>
2022-07-18 00:17:16 +0800

    单例模式

---
+++ leetcode/src/main/java/com/hrbu/designpattern/singleton/ChocolateBoiler.java
@@ -0,0 +1,31 @@
+package com.hrbu.designpattern.singleton;
+
+public class ChocolateBoiler {
+    private boolean empty;   // 锅炉是否是空的
+    private boolean boiled;  // 是否煮沸过
+    private static ChocolateBoiler chocolateBoiler;
+
+    private ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+
+    public synchronized static ChocolateBoiler getInstance() {
+        if (null == chocolateBoiler) {
+            chocolateBoiler =  new ChocolateBoiler();
+        }
+        return chocolateBoiler;
+    }
+
+    public void  fill() {
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+            // 在锅炉里填充巧克力和牛奶的混合物
+        }
+    }
+
+    private boolean isEmpty() {
+        return empty;
+    }
+}
