304a9fc73b69bae5a74b8a8153cca663b653909a
晚安 <a1033078928@gmail.com>
2022-07-18 23:39:51 +0800

    命令模式学习

--- leetcode/src/main/java/com/hrbu/designpattern/command/Command.java
+++ leetcode/src/main/java/com/hrbu/designpattern/command/Command.java
@@ -4,10 +4,11 @@ public interface Command {
     public void execute();
 }
 
-// 电灯类
-class Light {
-    public void on() {
-        System.out.println("灯被打开");
+class NoCommand implements Command {
+
+    @Override
+    public void execute() {
+        // System.out.println("什么也不做");
     }
 }
 
@@ -22,5 +23,53 @@ class LightOnCommand implements Command {
     @Override
     public void execute() {
         light.on();
+    }
+}
+
+// 关闭电灯
+class LightOffCommand implements Command {
+    Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.off();
+    }
+}
+
+/**
+ * 打开音响命令
+ */
+class StereoOnWithCDCommand implements Command {
+    Stereo stereo;
+
+    public StereoOnWithCDCommand(Stereo stereo) {
+        this.stereo = stereo;
+    }
+
+    @Override
+    public void execute() {
+        stereo.on();
+        stereo.setCD();
+        stereo.setVolume(11);
+    }
+}
+
+/**
+ * 关闭音响命令
+ */
+class StereoOffCommand implements Command {
+    Stereo stereo;
+
+    public StereoOffCommand(Stereo stereo) {
+        this.stereo = stereo;
+    }
+
+    @Override
+    public void execute() {
+        stereo.off();
     }
 }
--- leetcode/src/main/java/com/hrbu/designpattern/command/SimpleRemoteController.java
+++ leetcode/src/main/java/com/hrbu/designpattern/command/SimpleRemoteController.java
@@ -1,20 +1,62 @@
 package com.hrbu.designpattern.command;
 
+/**
+ * 遥控器类
+ */
 public class SimpleRemoteController {
     // 插槽持有命令，控制着一个装置
-    Command slot;
+    // 用来记录 7 个接口的命令
+    Command[] onCommands;
+    Command[] offCommands;
 
-    /**
-     * 用来设置插槽控制命令，可以通过多次调用改变行为
-     */
-    public void setCommand(Command command) {
-        slot = command;
+    public SimpleRemoteController() {
+        // 实例化并初始化两个开关数组
+        this.onCommands = new Command[7];
+        this.offCommands = new Command[7];
+
+        NoCommand noCommand = new NoCommand();
+        for (int i = 0; i < 7; i++) {
+            onCommands[i] = noCommand;
+            offCommands[i] = noCommand;
+        }
     }
 
     /**
-     * 按下按钮，命令衔接插槽，调用 execute() 方法
+     * @param slot   插槽位置
+     * @param onCommand   开命令
+     * @param offCommand  关命令
+     * 用来设置插槽控制命令，可以通过多次调用改变行为
      */
-    public void buttonWasPressed() {
-        slot.execute();
+    public void setCommand(int slot, Command onCommand, Command offCommand) {
+        onCommands[slot] = onCommand;
+        offCommands[slot] = offCommand;
+    }
+
+    /**
+     * 按下开启按钮，命令衔接插槽，调用 execute() 方法
+     */
+    public void onButtonWasPressed(int slot) {
+        onCommands[slot].execute();
+    }
+
+    public void offButtonWasPressed(int slot) {
+        offCommands[slot].execute();
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer stringBuffer = new StringBuffer();
+
+        for (int i = 0; i < 7; i++) {
+            stringBuffer.append("装置：")
+                    .append(i)
+                    .append("\t开启类：")
+                    .append(onCommands[i].getClass().getName())
+                    .append("\t关闭类：")
+                    .append(offCommands.getClass().getName())
+                    .append("\n");
+        }
+
+        return stringBuffer.toString();
     }
 }
