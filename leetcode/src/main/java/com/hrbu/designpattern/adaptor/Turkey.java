2c8cc2d7c0028dc5185a20d5eb6fdb5a27214591
晚安 <a1033078928@gmail.com>
2022-07-20 23:38:32 +0800

    适配器模式学习

---
+++ leetcode/src/main/java/com/hrbu/designpattern/adaptor/Turkey.java
@@ -0,0 +1,25 @@
+package com.hrbu.designpattern.adaptor;
+
+/**
+ * 火鸡接口
+ */
+public interface Turkey {
+    public void gobble();   // 火鸡咯咯叫
+    public void fly();      // 火鸡飞
+}
+
+/**
+ * 火鸡实现
+ */
+class WildTurkey implements Turkey {
+
+    @Override
+    public void gobble() {
+        System.out.println("火鸡咯咯叫");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("火鸡飞行");
+    }
+}
